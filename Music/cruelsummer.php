892efe87e332f5e42e50a80c3b09f1fe394a1545
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-04-07 17:05:38 -0400

    CruelSUmmer

--- Music/cruelsummer.php
+++ Music/cruelsummer.php
@@ -16,22 +16,23 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Music - BandNet</title>
-    <link rel="stylesheet" href="newstyle.css">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="/newstyle.css">
+    <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <!-- Unicons CSS -->
-    <link rel="stylesheet" href="line.css">
-   <script src="script.js" defer=""></script>
+    <link rel="stylesheet" href="/line.css">
+   <script src="/script.js" defer=""></script>
   </head>
   <body>
     <nav class="nav">
       <i class="uil uil-bars navOpenBtn"></i>
             <div class="logo">
-            <img src="<?php echo empty($_SESSION["school"]) ? 'logo.png' : htmlspecialchars($_SESSION["school"]) . '.png'; ?>" alt="Logo">
+            <img src="<?php echo empty($_SESSION["school"]) ? '/logo.png' : '/' . htmlspecialchars($_SESSION["school"]) . '.png'; ?>" alt="Logo">
+
             </div>
       <ul class="nav-links">
         <i class="uil uil-times navCloseBtn"></i>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="music.php">Music</a></li>
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/music.php">Music</a></li>
         <li><p class="welcomeName">Welcome, <?php echo htmlspecialchars($_SESSION["username"]); ?></p></li>
         </ul>
     </nav>
