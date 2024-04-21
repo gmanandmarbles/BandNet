0450c9d5fa365cb1eca9d50a6395cd886bd03068
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2024-04-20 21:20:46 -0400

    Update music.php

--- music.php
+++ music.php
@@ -44,30 +44,38 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             <tr>
                 <th>Title</th>
                 <th>Artist</th>
+                <th>Instruments</th>
             </tr>
             <tr>
                 <td><a href="Music\cruelsummer.php">Cruel Summer</a></td>
                 <td>Taylor Swift</td>
+                <td>Flute, Clarinet, Bass clarinet, Alto saxophone, Tenor saxophone, Baritone saxophone, Trumpet, Mellophone, Trombone, Bariton, Sousaphone, Snare drum, Tenor drums, Bass drums.</td>
             </tr>
             <tr>
                 <td><a href="Music\fastcar.php">Fast Car</a></td>
                 <td>Luke Combs</td>
+                <td>Flute, Clarinet, Alto saxophone, Tenor saxophone, Baritone saxophone, Trumpet, Mellophone, Trombone, Baritone, Sousaphone, Snare drum, Tenor drum, Bass drum.</td>
             </tr>
             <tr>
                 <td><a href="Music\feelinggood.php">Feeling Good</a></td>
                 <td>Michael Buble</td>
+                <td>Trumpet, Alto saxophone, Tenor saxophone, Baritone saxophone, Trombone, Bass trombone, Electric guitar, Piano, Contrabass.</td>
             </tr>
             <tr>
                 <td><a href="Music\gdfr.php">GDFR</a></td>
                 <td>Flo Rida</td>
+                <td>Piccolo, Clarinet, Bass clarinet, Alto saxophone, Tenor saxophone, Baritone saxophone, Trumpet, Horn, Trombone, Tuba, Drumset, Stamp.</td>
+
             </tr>
             <tr>
                 <td><a href="Music\IAINTWORRIED.php">I Ain't Worried</a></td>
                 <td>OneRepublic</td>
+                <td>Piccolo, Flute, Clarinet, Bass clarinet, Alto saxophone, Tenor saxophone, Baritone saxophone, Trumpet, Mellophone, Horn in F, Trombone, Baritone, Tuba, Sousaphone, Glockenspiel, Xylophone.</td>
             </tr>
             <tr>
                 <td><a href="Music\kungfufighting.php">Kung Fu Fighting</a></td>
                 <td>Carl Douglas</td>
+                <td>Alto saxophone, Tenor saxophone, Baritone saxophone, Trumpet, Trombone, Bass trombone, Drumset, Piano, Bass guitar. </td>
             </tr>
             
         </table>
